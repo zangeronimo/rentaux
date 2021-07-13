915df8876d4f38f02c8e2ab8598b2ebb3d95f274
Luciano Zangeronimo <zangeronimo@gmail.com>
2021-07-13 20:09:14 -0300

    rota para categories

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
 
 const port = 4000;
 app.listen(port, () =>
